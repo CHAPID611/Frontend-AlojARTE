fa05bc5d7469284aa5247729f08ff13c5695e5a5
CHAPID611 <nenfarchapid@gmail.com>
2025-04-11 16:26:32 -0500

    agregando proveedor HttpClient, lazyLoading, renderizado de servidor en main.ts y main.server.ts y creando el ambiente de alacenamiento de url de backend en environment.ts para la conexion con el backend

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideServerRendering } from '@angular/platform-server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideServerRendering(), // Proveedor para renderizado en servidor
+    provideHttpClient(),        // Proveedor para HttpClient
+    provideRouter(routes)       // Configuración de rutas
+  ]
+});
